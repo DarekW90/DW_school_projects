70319e55b7323ff8bdb6af5c1c21797a084dc5f7
Dariusz Walda <walda.darek@gmail.com>
2023-05-10 19:54:48 +0200

    few changes

---
+++ kalkulator/kalkulator/calculator.cs
@@ -0,0 +1,15 @@
+namespace kalkulator
+{
+    public partial class calculator : Form
+    {
+        public calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
