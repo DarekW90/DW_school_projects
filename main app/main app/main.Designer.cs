cbfe36d89246839753cad6d1fb16ff98619393ca
Dariusz Walda <walda.darek@gmail.com>
2023-05-11 22:03:45 +0200

    add gradient efect for mainApp

--- main app/main app/main.Designer.cs
+++ main app/main app/main.Designer.cs
@@ -30,44 +30,91 @@
         {
             btnCalculator = new Button();
             btnBreakOut = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // btnCalculator
             // 
-            btnCalculator.Location = new Point(49, 32);
+            btnCalculator.Location = new Point(56, 157);
             btnCalculator.Name = "btnCalculator";
-            btnCalculator.Size = new Size(474, 29);
+            btnCalculator.Size = new Size(250, 30);
             btnCalculator.TabIndex = 0;
-            btnCalculator.Text = "Calculator";
+            btnCalculator.Text = "Kalkulator";
             btnCalculator.UseVisualStyleBackColor = true;
             btnCalculator.Click += btnForm2_Click;
             // 
             // btnBreakOut
             // 
-            btnBreakOut.Location = new Point(49, 95);
+            btnBreakOut.Location = new Point(449, 157);
             btnBreakOut.Name = "btnBreakOut";
-            btnBreakOut.Size = new Size(474, 29);
+            btnBreakOut.Size = new Size(250, 30);
             btnBreakOut.TabIndex = 1;
-            btnBreakOut.Text = "Gra: Break Out";
+            btnBreakOut.Text = "Break Out";
             btnBreakOut.UseVisualStyleBackColor = true;
             btnBreakOut.Click += btnBreakOut_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(271, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(205, 41);
+            label1.TabIndex = 2;
+            label1.Text = "Dariusz Walda";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(122, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 31);
+            label2.TabIndex = 3;
+            label2.Text = "Aplikacje";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(548, 113);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 31);
+            label3.TabIndex = 3;
+            label3.Text = "Gry";
+            label3.Click += label3_Click;
             // 
             // mainApp
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.White;
+            ClientSize = new Size(782, 453);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(btnBreakOut);
             Controls.Add(btnCalculator);
             Name = "mainApp";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Main";
+            Paint += mainApp_paint;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btnCalculator;
         private Button btnBreakOut;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- main app/main app/main.cs
+++ main app/main app/main.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Drawing2D;
+
 namespace main_app
 {
     public partial class mainApp : Form
@@ -17,6 +19,37 @@ namespace main_app
         {
             BreakOut breakOutGame = new BreakOut();
             breakOutGame.ShowDialog();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mainApp_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mainApp_paint(object sender, PaintEventArgs e)
+        {
+            Graphics mgraphics = e.Graphics;
+            Pen pen = new Pen(Color.FromArgb(96, 155, 173), 1);
+
+            Rectangle area = new Rectangle(0, 0, this.Width, this.Height - 1);
+            LinearGradientBrush lgb = new LinearGradientBrush(area, Color.FromArgb(96, 155, 233), Color.FromArgb(245, 251, 251), LinearGradientMode.Vertical);
+            mgraphics.FillRectangle(lgb, area);
+            mgraphics.DrawRectangle(pen, area);
         }
     }
 }
